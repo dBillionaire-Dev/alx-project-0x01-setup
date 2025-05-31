a84a145fadbd1807bff7be065ef02d5ee0990b8b
dBillioniare-Dev <nezerekunke@gmail.cm>
2025-05-31 06:54:09 +0100

    task_3

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,22 +1,38 @@
 import React from 'react'
 import Header from '@/components/layout/Header'
+import UserCard from "@/components/common/UserCard";
+import { UserProps } from "@/interfaces";
 
-const index = () => {
+const Users: React.FC<UserProps[]> = ({ users }) => {
     return (
-        <div>
+        <div className="flex flex-col h-screen">
             <Header />
-            <main className="flex-grow flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600">
-                <div className="text-center">
-                    <h1 className="text-5xl font-bold text-white">
-                        Welcome to the Posts Page!
-                    </h1>
-                    <p className="mt-4 text-xl text-white">
-                        Here you can find all the latest posts.
-                    </p>
+            <main className="p-4">
+                <div className="flex justify-between">
+                    <h1 className=" text-2xl font-semibold">User Content</h1>
+                    <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
+                </div>
+                <div className="grid grid-cols-3 gap-2 ">
+                    {
+                        users?.map(({ name, username, email, address }: UserProps, key: number) => (
+                            <UserCard name={name} username={username} email={email} address={address} key={key} />
+                        ))
+                    }
                 </div>
             </main>
         </div>
     )
 }
 
-export default index
+export async function getStaticProps() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+    const posts = await response.json()
+
+    return {
+        props: {
+            posts
+        }
+    }
+}
+
+export default Users;
